6978f5ef5faf69eb2f40843dc968a52c5aad02e4
utkarshmaske88 <101203306+utkarshmaske88@users.noreply.github.com>
2022-09-29 23:21:20 +0530

    Update App.js
    all blogs deleted

--- src/App.js
+++ src/App.js
@@ -7,48 +7,7 @@ import Blogs from "./components/blogs";
 import "./App.css";
 
 let defaultBlogList = [
-  {
-    id: "uniqueId-1",
-    title: "Blog Title",
-    subtitle: "Sub Title",
-    content: "This is going to be very long line.",
-    author: "Username of the user",
-  },
-  {
-    id: "uniqueId-2",
-    title: "Blog Title",
-    subtitle: "Sub Title",
-    content: "This is going to be very long line.",
-    author: "Username of the user",
-  },
-  {
-    id: "uniqueId-3",
-    title: "Blog Title",
-    subtitle: "Sub Title",
-    content: "This is going to be very long line.",
-    author: "Username of the user",
-  },
-  {
-    id: "uniqueId-4",
-    title: "Blog Title",
-    subtitle: "Sub Title",
-    content: "This is going to be very long line.",
-    author: "Username of the user",
-  },
-  {
-    id: "uniqueId-5",
-    title: "Blog Title",
-    subtitle: "Sub Title",
-    content: "This is going to be very long line.",
-    author: "Username of the user",
-  },
-  {
-    id: "uniqueId-6",
-    title: "Blog Title",
-    subtitle: "Sub Title",
-    content: "This is going to be very long line.",
-    author: "Username of the user",
-  },
+  
 ];
 
 function App() {
